3843bc408699986c24cebf6c01c0cc8fc72228cc
ntaneja1985 <93190220+ntaneja1985@users.noreply.github.com>
2024-11-11 00:29:01 +0530

    Added the Product Details Page
    Added the Product Details Page

---
+++ WebApp/WebApp/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Route("product")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("productDetails")]
+        public IActionResult GetProductInfo()
+        {
+            ViewBag.Title = "My Product Page";
+            ProductViewModel productViewModel = new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Test",
+                Description = "Test",
+                Price = 1,
+            };
+            return View(productViewModel);
+        }
+
+
+    }
+}
